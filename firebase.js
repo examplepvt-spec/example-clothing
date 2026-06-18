4b2f093b3b842b18884813e5809d23ba0dd13fef
examplepvt-spec <examplepvt@gmail.com>
2026-06-18 16:12:59 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBE4RSDLkGFclHv-90eCPZlwSCvaKr27Po",
+  authDomain: "example-clothing.firebaseapp.com",
+  projectId: "example-clothing",
+  storageBucket: "example-clothing.firebasestorage.app",
+  messagingSenderId: "365541402622",
+  appId: "1:365541402622:web:129d716d298bb2de9342af",
+  measurementId: "G-MQS6NQF4ZG"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
