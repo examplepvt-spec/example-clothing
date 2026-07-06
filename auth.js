6b812d590ba70e3962b3fbd452d998c3acb0f04d
examplepvt-spec <examplepvt@gmail.com>
2026-07-06 17:44:17 +0530

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,67 @@
+import { auth, db } from "./firebase.js";
+
+import {
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword,
+signOut,
+onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+doc,
+setDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+// REGISTER
+export async function register(name, email, password){
+
+const userCredential =
+await createUserWithEmailAndPassword(
+auth,
+email,
+password
+);
+
+await setDoc(
+doc(db,"customers",userCredential.user.uid),
+{
+name:name,
+email:email,
+createdAt:new Date().toISOString()
+});
+
+return userCredential.user;
+
+}
+
+// LOGIN
+export async function login(email,password){
+
+const userCredential =
+await signInWithEmailAndPassword(
+auth,
+email,
+password
+);
+
+return userCredential.user;
+
+}
+
+// LOGOUT
+export async function logout(){
+
+await signOut(auth);
+
+}
+
+// CURRENT USER
+export function checkUser(callback){
+
+onAuthStateChanged(auth,(user)=>{
+
+callback(user);
+
+});
+
+}
